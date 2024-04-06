7da478781410a5d737f8975d2e93b72d8317069c
Pavel Karelin <paha.karelinn@gmail.com>
2024-04-06 19:51:17 +0800

    improve formating

--- cpp/DadHelpsLoseWeight.cpp
+++ cpp/DadHelpsLoseWeight.cpp
@@ -1,10 +1,5 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define ld long double
-#define pb push_back
-#define lli unsigned long long int
-#define F first
-#define S second
 using namespace std;
 const ll L = 1e9+7;
 vector<ll> div(ll n){
@@ -18,7 +13,7 @@ vector<ll> div(ll n){
     ans.pb(n);
     return ans;
 }
-int main() {   
+int main() {
     cin.tie(nullptr); cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
@@ -46,5 +41,4 @@ int main() {
     }
     if(C && v[1]-v[0] != 1 && v[1]-v[2] != 0) cout << "1\n";
     else cout << "2\n";
-   
 }
